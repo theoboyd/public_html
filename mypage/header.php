455e2567988bc0cb2608d2da920b4b57609b124b
theoboyd <theoboyd@gmail.com>
2019-10-10 18:05:43 +0900

    Create header.php

---
+++ mypage/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <?php
+  header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+  header("Cache-Control: post-check=0, pre-check=0", false);
+  header("Pragma: no-cache");
+  ?>
+  <head>
+    <title>Labelling Experiment</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="styles/main.css" />
+    <link rel="stylesheet" type="text/css" href="styles/columns.css" />
+  </head>
+  <body>
+      <div id="wrap">
+      <div id="main">
+        <div id="leftcontent">
+          <header class="body">
+            <h2>Labelling Experiment</h2>
+          </header>
+          <section id="content" class="body">
