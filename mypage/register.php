f7fb3b1c362aa927af576764ae8d436ce08de413
theoboyd <theoboyd@gmail.com>
2019-10-10 18:03:30 +0900

    Create register.php

---
+++ mypage/register.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+<h3>Please register:</h3>
+<p></p>
+<form action="login.php" method="post">
+  Name: <input type="text" name="user">
+  <input type="submit">
+</form>
+<?php include 'footer.php'; ?>
